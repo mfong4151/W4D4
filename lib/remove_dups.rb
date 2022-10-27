93a7e2f991fd68ff12b2b67f6fc4551924ca3d5c
Andrea Cano <andreacanogisbert@gmail.com>
2022-10-27 14:46:50 -0700

    two_sum array finish

--- lib/remove_dups.rb
+++ lib/remove_dups.rb
@@ -7,4 +7,20 @@ class Array
         end
         res
     end
+
+    def two_sum 
+        pos = {}
+        res = []
+
+        (0...self.length).each do |i|
+            if pos.has_key?(self[i]) 
+                res << [pos[self[i]], i]
+            end
+            pos[-self[i]] = i 
+        end
+
+        res.sort
+    end
+
+    
 end
--- spec/remove_dups_spec.rb
+++ spec/remove_dups_spec.rb
@@ -8,7 +8,25 @@ describe "uniq" do
 
     it "#removes duplicates from an array" do
         expect(array.uniq).to eq([1,2,3,4,5])
-
     end
+end
+
+describe "two_sum" do 
+    let(:array1) {[-1, 0, 2, -2, 1]}
+    let(:array2) {[-5,  4, 3 , 5, 5]}
+    let (:array3) {[]}
+
+    it "return all pairs of positions where the elements at those positions sum to zero" do 
+        expect(array1.two_sum).to eq([[0,4],[2,3]])   
+    end
+
+    it "return all multiple positions that belong to the same indice" do 
+        expect(array2.two_sum).to eq([[0, 3],[0,4]])
+    end
+
+    it "return empty array if length < 2" do
+        expect(array3.two_sum).to eq([])
+    end
+
 end
 
